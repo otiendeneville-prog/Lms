7f8d5372d4869234a593aa09a0ba70882aea4f94
Neville <otiendeneville@gamil.com>
2026-03-24 07:05:12 +0300

    adding new file heelo.jsx

---
+++ app/(tabs)/hello.jsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const hello = () => {
+  return (
+    <View>
+      <Text>hello world in react native app development in the situation of the nation suffering</Text>
+    </View>
+  )
+}
+
+export default hello
+
+const styles = StyleSheet.create({})
