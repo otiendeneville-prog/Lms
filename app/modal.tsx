fbfb4262b8732a2b72766f46b5153a2a70d8f761
Neville <otiendeneville@gamil.com>
2026-03-31 11:52:55 +0300

    lms change

--- app/modal.tsx
+++ app/modal.tsx
@@ -1,10 +1,12 @@
 import { View, Text,StyleSheet, } from 'react-native'
+import { ScrollView } from 'react-native-reanimated/lib/typescript/Animated'
 import React from 'react'
 
 const modal = () => {
   return (
     <View style={styles.View}>
       <Text style={StyleSheet.Text}>modal Page</Text>
+
     </View>
   )
 }
